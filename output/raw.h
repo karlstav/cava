e746d52c339c8de2aa859b4618a1bc2808616baa
karl <karl@stavestrand.no>
2017-06-28 10:52:20 +0200

    Revert "all: remove include *.c from cava.c"

--- output/raw.h
+++ output/raw.h
@@ -1,5 +1,2 @@
-#include "common.h"
-
 int print_raw_out(int bars_count, int fd, int is_binary, int bit_format,
-	int ascii_range, char bar_delim, char frame_delim,
-	const int f[FRAMES_BUFFER_SIZE]);
+	int ascii_range, char bar_delim, char frame_delim, const int const f[200]);
--- output/terminal_noncurses.h
+++ output/terminal_noncurses.h
@@ -1,8 +1,4 @@
-#include "common.h"
-
 int init_terminal_noncurses(int col, int bgcol, int w, int h, int bw);
 void get_terminal_dim_noncurses(int *w, int *h);
-int draw_terminal_noncurses(int virt, int height, int width, int bars, int bw,
-	int bs, int rest, int f[FRAMES_BUFFER_SIZE],
-	int flastd[FRAMES_BUFFER_SIZE]);
+int draw_terminal_noncurses(int virt, int height, int width, int bars, int bw, int bs, int rest, int f[200], int flastd[200]);
 void cleanup_terminal_noncurses(void);
