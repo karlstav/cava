fc2e5715816479df3d01d2c6cbd0473d126eaff4
karlstav <karl@stavestrand.no>
2022-03-27 15:59:53 +0200

    update cavacore test app

--- cavacore_test.c
+++ cavacore_test.c
@@ -14,7 +14,14 @@ void main() {
     printf("welcome to cavalib standalone test app\n");
     printf("planning cava 20 bars (left+right) 44100 rate 2 cahnnels, 100 target height, 60 "
            "framerate \n");
-    struct cava_plan *plan = cava_init(20, 44100, 2, 100, 60);
+
+    int bars_per_channel = 10;
+    int channels = 2;
+    int height = 100;
+    int framerate = 60;
+    int rate = 44100;
+
+    struct cava_plan *plan = cava_init(bars_per_channel, rate, channels, height, framerate);
 
     printf("got lower cut off frequencies:\n");
 
@@ -25,36 +32,35 @@ void main() {
 
     printf("allocating buffers and generating sine wave for test\n\n");
 
-    int *cava_out;
-    int32_t *cava_in;
+    double *cava_out;
+    double *cava_in;
 
-    cava_out = (int *)malloc(1024 * 2 * sizeof(int));
+    cava_out = (double *)malloc(BUFFER_SIZE * 2 * sizeof(double));
 
-    cava_in = (int32_t *)malloc(1024 * 2 * 4 * sizeof(int32_t));
+    cava_in = (double *)malloc(BUFFER_SIZE * 2 * 4 * sizeof(double));
 
-    for (int i = 0; i < 1024 * 2; i++) {
+    for (int i = 0; i < BUFFER_SIZE * 2; i++) {
         cava_out[i] = 0;
     }
-    for (int n = 0; n < 4096; n++) {
+    for (int n = 0; n < BUFFER_SIZE * 4; n++) {
         cava_in[n * 2] = sin(2 * PI * 200 / 44100 * n) * 10000;
         cava_in[n * 2 + 1] = sin(2 * PI * 2000 / 44100 * n) * 10000;
     }
 
-    printf("running cava execute 40 times\n\n");
-    for (int k = 0; k < 40; k++) {
-        cava_execute(cava_in, 8192, cava_out, plan);
+    printf("running cava execute 300 times (simulating 5 seconds run time)\n\n");
+    for (int k = 0; k < 300; k++) {
+        cava_execute(cava_in, BUFFER_SIZE * 4 * 2, cava_out, plan);
     }
 
-    printf("output right,  max value should be at 200Hz:\n");
-
+    printf("\noutput left, max value should be at 2000Hz:\n");
     for (int i = 0; i < 10; i++) {
-        printf("%d \t", cava_out[i]);
+        printf("%.0f \t", cava_out[i]);
     }
     printf("MHz\n");
-    printf("\noutput left, max value should be at 2000Hz:\n");
 
+    printf("output right,  max value should be at 200Hz:\n");
     for (int i = 10; i < 20; i++) {
-        printf("%d \t", cava_out[i]);
+        printf("%.0f \t", cava_out[i]);
     }
     printf("MHz\n");
     cava_destroy(plan);
