d637cb072cff64a4aad6af6a012e722b5b93bb79
karlstav <karl@stavestrand.no>
2022-04-18 20:48:31 +0200

    discard input buffer if overflowing

--- input/common.c
+++ input/common.c
@@ -9,7 +9,11 @@ int write_to_cava_input_buffers(int16_t size, int16_t buf[size], void *data) {
     struct audio_data *audio = (struct audio_data *)data;
 
     pthread_mutex_lock(&audio->lock);
-    if (audio->samples_counter + size > audio->input_buffer_size * 4) {
+    if (audio->samples_counter + size > audio->cava_buffer_size) {
+        // buffer overflow, discard what ever is in the buffer and start over
+        for (uint16_t n = 0; n < audio->cava_buffer_size; n++) {
+            audio->cava_in[n] = 0;
+        }
         audio->samples_counter = 0;
     }
     for (uint16_t i = 0; i < size; i++) {
