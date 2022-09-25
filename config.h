fb3e14119d943060d29215b896ea8b28a32197a7
MaciejDziuban <dziuban.maciej@gmail.com>
2022-09-25 09:01:09 +0200

    Reformat code

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ enum data_format { FORMAT_ASCII = 0, FORMAT_BINARY = 1, FORMAT_NTK3000 = 2 };
 
 enum xaxis_scale { NONE, FREQUENCY, NOTE };
 
-enum orientation { ORIENT_BOTTOM, ORIENT_TOP, ORIENT_LEFT, ORIENT_RIGHT};
+enum orientation { ORIENT_BOTTOM, ORIENT_TOP, ORIENT_LEFT, ORIENT_RIGHT };
 
 struct config_params {
     char *color, *bcolor, *raw_target, *audio_source,
