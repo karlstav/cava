746a3b1e6021e383aea9d0000f49d71fb24e1856
karl <karl@stavestrand.no>
2022-11-04 09:01:05 +0100

    bump hardcoded version to 0.8.3

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 if [ -d .git ]; then
   git describe --always --tags --dirty > version # get version from git
 else
-  echo 0.8.2 > version # hard coded versions
+  echo 0.8.3 > version # hard coded versions
 fi
 
 libtoolize
