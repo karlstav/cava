b28d8674be820e1b51626d15a0fd68c4c3b0d9d9
bsdcode <155195419+bsdcode@users.noreply.github.com>
2024-01-20 01:00:01 +0100

    JACK: add autoconnect option for automatic port connections

--- config.h
+++ config.h
@@ -111,8 +111,8 @@ struct config_params {
     enum orientation orientation;
     int userEQ_keys, userEQ_enabled, col, bgcol, autobars, stereo, raw_format, ascii_range,
         bit_format, gradient, gradient_count, fixedbars, framerate, bar_width, bar_spacing,
-        bar_height, autosens, overshoot, waves, samplerate, samplebits, channels, sleep_timer,
-        sdl_width, sdl_height, sdl_x, sdl_y, sdl_full_screen, draw_and_quit, zero_test,
+        bar_height, autosens, overshoot, waves, samplerate, samplebits, channels, autoconnect,
+        sleep_timer, sdl_width, sdl_height, sdl_x, sdl_y, sdl_full_screen, draw_and_quit, zero_test,
         non_zero_test, reverse, sync_updates, continuous_rendering, disable_blanking;
 };
 
