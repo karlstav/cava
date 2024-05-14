a2f314df4b6c6bf8b6800124ed185b5a2ef50288
karl <karl@stavestrand.no>
2024-05-14 20:24:24 +0200

    bump hardcoded version to 0.10.2

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 if [ -d .git ]; then
   git describe --always --tags --dirty > version # get version from git
 else
-  echo 0.10.1 > version # hard coded versions
+  echo 0.10.2 > version # hard coded versions
 fi
 
 libtoolize
