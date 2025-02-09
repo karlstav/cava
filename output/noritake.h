0da1d72239a9ec6ffa93abe5d755bb3e96062369
Nick Logozzo <nlogozzo225@gmail.com>
2025-02-08 22:41:12 -0500

    fix: Windows build

--- output/noritake.h
+++ output/noritake.h
@@ -2,7 +2,7 @@
 int print_ntk_out(int bars_count, int fd, int bit_format, int bar_width, int bar_spacing,
                   int bar_height, int const f[]);
 #else
-#Include "Windows.h"
+#include "Windows.h"
 int print_ntk_out(int bars_count, HANDLE hFile, int bit_format, int bar_width, int bar_spacing,
                   int bar_height, int const f[]);
 #endif
