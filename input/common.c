90ae22da1593ca3619df99b752c28dfc41ed4ae4
pallaswept <132128962+pallaswept@users.noreply.github.com>
2025-08-03 23:58:28 +1000

    Set samples_counter on reset
    Set samples_counter on reset_output_buffers() to trigger cavacore to read in the empty buffer.

--- input/common.c
+++ input/common.c
@@ -51,6 +51,7 @@ void reset_output_buffers(struct audio_data *data) {
     for (uint16_t n = 0; n < audio->cava_buffer_size; n++) {
         audio->cava_in[n] = 0;
     }
+    audio->samples_counter = audio->cava_buffer_size;
     pthread_mutex_unlock(&audio->lock);
 }
 
