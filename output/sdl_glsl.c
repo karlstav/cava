5d0be79c92fc9bcefc7e0b8a20cd6ac45e2f80cd
rezky_nightky <with.rezky@gmail.com>
2026-01-15 14:09:07 +0700

    fix: Fix applied clang-format-lint diff in output/sdl_glsl.c
    I updated output/sdl_glsl.c to match the exact clang-format-9 expectations from your CI output:
    
    Include order: moved #include <stdint.h> above #include <stdlib.h>
    Line wrap: reformatted the glTexImage2D(...) call onto a single line (as in the CI “reformatted” output)
    
    Signed-off-by: rezky_nightky <with.rezky@gmail.com>

--- output/sdl_glsl.c
+++ output/sdl_glsl.c
@@ -10,8 +10,8 @@
 
 #include <math.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "../util.h"
 
@@ -159,8 +159,7 @@ void init_sdl_glsl_window(int width, int height, int x, int y, int full_screen,
         // Create a texture to attach to the framebuffer
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                     NULL);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
